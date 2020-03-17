d89f842d82a25f1b539bf081ca76cf95308c1161
germanr8 <german.reyga@gmail.com>
2020-03-16 20:27:43 -0600

    POST method handled with AJAX

--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -2,8 +2,7 @@ const Task = require("../models/Task");
 
 exports.store = (req, res) => {
   let task = {};
-  task.description = req.body;
-  console.log(req.body.description);
+  task.description = req.body.description;
   Task.create(task).then(id => {
     if (req.xhr || req.headers.accept.indexOf("json") > -1) {
       Task.find(id).then(task => res.json(task));
